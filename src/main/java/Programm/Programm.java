056d2c690201a9e7d1c94320e5a53f18155a28e4
Danila143 <danila143@gmail.com>
2016-05-22 10:57:46 +0300

    Create First Project

---
+++ src/main/java/Programm/Programm.java
@@ -0,0 +1,22 @@
+package Programm;
+
+import java.awt.*;
+
+/**
+ * Created by Danila on 22.05.2016.
+ */
+
+public class Programm {
+    /*public static void buildInvite(){
+        Frame f = new Frame();
+        Label I = new Label("Вечеринка у Тима");
+        Button b = new Button("Ваша ставка");
+        Button c = new Button("Сброс");
+        Panel p = new Panel();
+        p.add(I);
+    }*/
+
+    public static void main(String[] args) {
+        //buildInvite();
+    }
+}
