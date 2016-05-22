4a7e441a926a2e98ce04479f45995521f79b0c3d
Danila143 <danila143@gmail.com>
2016-05-22 10:54:52 +0300

    Create First Project

---
+++ src/main/java/Programm/Danila.java
@@ -0,0 +1,12 @@
+package Programm;
+
+/**
+ * Created by Danila on 22.05.2016.
+ */
+
+public class Danila {
+    public static void main(String[] args) {
+        System.out.println("Danila hello");
+        System.out.println();
+    }
+}
